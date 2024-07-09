6e5e9e002d96e4267a168046287b4a73fe130e72
Andrea Borelli <andreaborelli@blu.it>
2024-07-09 11:45:36 +0200

    Le costanti: const - Mutabilità vs Immutabilità

--- script.js
+++ script.js
@@ -5,3 +5,15 @@ function name(){
     console.log(names);
   }
   name()
+
+  
+const obj = {id: 123, name: "Andrea"}
+
+obj.id = 909090
+obj.colore ="red";
+
+console.log(obj);
+
+let data = [10, 20, 30, 40];
+data = data.concat([50, 60, 70, 80])
+console.log(data);
