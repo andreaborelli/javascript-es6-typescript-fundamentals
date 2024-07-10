8096ebff59dd89c98fb8920f6c01d5db3f259ec5
Andrea Borelli <andreaborelli@blu.it>
2024-07-10 19:07:06 +0200

    Destructuring Array

--- script.js
+++ script.js
@@ -1,27 +1,33 @@
-/* Short Object syntax: la possibilità di creare oggetti 
-con una sintassi molto più coincisa */
+/* il Destructuring Array o destrutturazione 
+è una nuova espressione introdotta in javascript ES6 
+per estrarre da array o da oggetti dei valori rispettivamente 
+o delle proprietà creando delle nuove variabili 
+che conterranno tali valori */
 
-const user = {
-  id: 123,
-  name: 'Andrea',
-  surname: 'Borelli',
-  profile: {
-    color: 'blue',
-    location: 'Genova'
-  }
+const list = [10, 20, 30, 40, 50];
 
-};
+// const [a, b, c, d, e = 50] = list; // ad variabile e assegno un dato di default
 
-/* const params = {
-	id: user.id, // recupera valori da user
-  	color: user.profile.color
-} */
+//const [a, b] = list;
 
+const [a, b, ...rest] = list; /* con destructuring invece di utilizzare il nome della costante 
+						direttamente subito dopo const, utilizzeremo le parentesi quadre 
+                        per indicare che stiamo facendo il destructuring di un array, 
+                        creando delle variabili a, e b, 
+                        dove a sarà assegnato automaticamente al primo valore dell'array, 
+                        e b al secondo valore dell'array, 
+                        semplicemente scrivendo il segno = 
+                        e definendo l'oggetto, list
+                        in questo caso l'array che vogliamo estrapolare tali proprietà
+                        sintassi più pulita */
+/* non sapendo la lunghezza dell'array, impostiamo i primi due valori, 
+è aggiungendo lo spread operator aggiungendo ... 
+con il nome di un ulteriore proprietà che potremmo chiamare come vogliamo es ...rest 
+così facendo recupereremo i valori rimanenti successivi ai primi 2 array */
 
-const id = user.id
-const color = user.profile.color
-
-const params = { id, color }
-
-// scambio dati server ajax
-console.log(params);
+// const a = list[0];
+// console.log(list[0]);
+// console.log(a);
+//console.log(a, b, c, d, e );
+//console.log(a, b, );
+console.log(a, b, rest );
