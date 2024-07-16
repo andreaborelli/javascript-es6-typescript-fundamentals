8a196991074e8477259a2d3eb254a9abf04661f6
Andrea Borelli <andreaborelli@blu.it>
2024-07-16 17:34:47 +0200

    Destructuring Object - part 3: rename & short object syntax

--- script.js
+++ script.js
@@ -1,5 +1,4 @@
-// oggetto che contiene diveri nodi al suo interno
-// Destructuring - Nested - nidificato
+// il rename è utile perchè ci permette di rinominare una variabile in un'altra variabile// rename & short object syntax
 
 const user = {
 	name: 'Andrea',
@@ -15,9 +14,31 @@ const user = {
 const { 
   profile: { 
     location: {
-      lat, lng, zoom = 5  // nuovo valoe zoom, settiamo un nuovo valore di deafault
+      lat: lt, lng: lg, zoom = 5  // nuovo valoe zoom, settiamo un nuovo valore di deafault
     } 
   } 
 } = user; // destruttura da profile una sotto proprietà, es. location
 
-console.log(lat, lng, zoom );
+
+const params = { lt, lg, zoom } 
+
+	// lt, // rimuoviamo : lt
+  	// lg,	// rimuoviamo : lg
+  	// zoom // rimuoviamo : zoom
+ /* short object syntax è possibile andare ad eliminare la parte in eccedenza 
+nel momento in cui chiave e valore hanno lo stesso nome quindi nel caso in cui lt ed lg
+coincidano possiamo rimuoverne uno dei due */
+
+// ajax
+
+console.log(params);
+
+/* 
+output:
+
+{
+  "lt": 15,
+  "lg": 12,
+  "zoom": 5
+}
+*/
