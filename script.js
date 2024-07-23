9d8c545998c24f04456523716893fd6b96a2717a
Andrea Borelli <andreaborelli@blu.it>
2024-07-23 19:05:43 +0200

    Object Spread operator vs Object.assign

--- script.js
+++ script.js
@@ -1,28 +1,37 @@
-/* lo spread operator è una nuova funzionalità 
-offerta da ES6 ed è rappresentata 
-da 3 punti uno dietro l'altro ... 
-è funziona sia con gli array che con gli oggetti*/
 
-// const data = [1, 2, 3, 4];
+/* per effettuare un clone di un oggetto o un merge 
+tra due oggetti in JavaScript abbiamo sostanzialmente due possibilità,
+1 è l'utilizzo di object.assign, e l'altro l'utilizzo di Object spread operator*/
+const obj = {
+	id: 123,
+  name: 'Andrea'
+}
 
-// const result = [data];
+const preferences = { color: 'red' };
+
+// const cloned = Object.assign({}, obj); // clone passando l'oggetto che vogliamo clonare
+
+// const cloned = Object.assign({}, obj, preferences, { pet: 'dog' }); 
+/* Object.assign accetta un numero arbitrario (non specificato) 
+di parametri, possiamo anche inserire un nuovo oggetto 
+es: Object.assign({}, obj, preferences { pet: 'dog' }) */
 
 
-const data = [1, 2, 3, 4];
+const merged = {...obj, ...preferences, pet: 'dog'}; /* Object Spread Operator, è la versione sintetica dell'object.assign:
+creiamo un nuovo oggetto è al posto di passare un array, 
+andremo con il ... è il nostro oggetto {...obj} 
+abbiamo anche la possibilità di fare il merge con es: preferences {...obj, ...preferences} 
+possiamo anche aggiungere altre proprietà aggiungendo pet: 'dog' così: {...obj, ...preferences, pet: 'dog'} */
 
-const list = [5, 6];
+// cloned.id = 456; 
+/* valore mutato infatti da lo stesso risulto se loggo obj, 
+quindi bisogna aggiungere le {} così: Object.assign({}, obj); 
+in pratica Object.assign crea un nuovo oggetto (reference) con {}, e fa il merge con obj
+ quindi con ({}, obj) facciamo un merge tra un oggetto vuoto e obj,
+ infatti aggiungendo preferences faremo il merge tra obj e preferences 
+ 
+ Immutabilità: Creare nuovi oggetti piuttosto che modificare quelli esistenti 
+ aiuta a mantenere l'immutabilità, che è una buona pratica nella programmazione 
+ funzionale.*/
 
-// al posto di usare push, usiamo concat data.push();
-
-// const result = data.concat([5]); 
-/* data.concat([5]); questo meccanismo ci permette di sfruttare 
-anche il concetto di immutabilità soprattutto in framework 
-come Angular e React */
-
-// const cloned =[...data, ...list, 7, 8]; // cloniamo array data
-
-// abbiamo creato un nuovo array che è un merge di data e lista e con l'aggiunta di due elementi in linea
-
-const merged =[...data, ...list, 7, 8]; // cloniamo array data
-
-console.log(merged);
+console.log(merged); // { "id": 123, "name": "Andrea", "color": "red", "pet": "dog"}
