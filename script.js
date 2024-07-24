9b163a1468c261f4ed667d75cd60678ce6087229
Andrea Borelli <andreaborelli@blu.it>
2024-07-24 18:40:00 +0200

    Arrow function

--- script.js
+++ script.js
@@ -1,37 +1,39 @@
+// 1 modo di scrittura funzione
+/*function add(a, b){
+/* return 'Hello';
+  return a + b;
+}
+add();
+console.log(add(3, 4));*/
 
-/* per effettuare un clone di un oggetto o un merge 
-tra due oggetti in JavaScript abbiamo sostanzialmente due possibilità,
-1 è l'utilizzo di object.assign, e l'altro l'utilizzo di Object spread operator*/
-const obj = {
-	id: 123,
-  name: 'Andrea'
+// 2 modo di scrittura funzione associandola ad una variabile
+/* var add = function addOperation(a, b){
+  return a + b;
+}
+add();
+console.log(add(3, 4));*/
+
+/* una funzione arrow è una funzione nella quale abbiamo 
+omesso la parola chiave function, 
+è abbiamo utilizzato dopo i parametri i segni di => per indicare una freccia, 
+il vantaggio è quello di essere una sintassi più coincisa, 
+ci permette dei vantaggi per l'utilizzo del this  */
+
+const add = (a, b) => {
+  return a + b;
 }
 
-const preferences = { color: 'red' };
+/* const divide = (a, b) => { 
+  return a / b; 
+} */
+/* quando il blocco di istruzioni ha solo una righa, 
+possiamo portare la riga in linea, togliere il return e le {}  */
 
-// const cloned = Object.assign({}, obj); // clone passando l'oggetto che vogliamo clonare
+const divide = (a, b) =>  a / b; // il return sarà implicito
 
-// const cloned = Object.assign({}, obj, preferences, { pet: 'dog' }); 
-/* Object.assign accetta un numero arbitrario (non specificato) 
-di parametri, possiamo anche inserire un nuovo oggetto 
-es: Object.assign({}, obj, preferences { pet: 'dog' }) */
-
-
-const merged = {...obj, ...preferences, pet: 'dog'}; /* Object Spread Operator, è la versione sintetica dell'object.assign:
-creiamo un nuovo oggetto è al posto di passare un array, 
-andremo con il ... è il nostro oggetto {...obj} 
-abbiamo anche la possibilità di fare il merge con es: preferences {...obj, ...preferences} 
-possiamo anche aggiungere altre proprietà aggiungendo pet: 'dog' così: {...obj, ...preferences, pet: 'dog'} */
-
-// cloned.id = 456; 
-/* valore mutato infatti da lo stesso risulto se loggo obj, 
-quindi bisogna aggiungere le {} così: Object.assign({}, obj); 
-in pratica Object.assign crea un nuovo oggetto (reference) con {}, e fa il merge con obj
- quindi con ({}, obj) facciamo un merge tra un oggetto vuoto e obj,
- infatti aggiungendo preferences faremo il merge tra obj e preferences 
- 
- Immutabilità: Creare nuovi oggetti piuttosto che modificare quelli esistenti 
- aiuta a mantenere l'immutabilità, che è una buona pratica nella programmazione 
- funzionale.*/
-
-console.log(merged); // { "id": 123, "name": "Andrea", "color": "red", "pet": "dog"}
+const pow = a => a * a; /* con un solo parametro possiamo omettere ' togliere ' le parentesi () in (a) 
+						così l'istruzione diventa ancora più coincisa */
+const greetings = () => console.log('Hello'); /* se non ci sono parametri nella funzione possiamo lasciare le () vuote, 
+												in alcuni casi invece di trovare le () possiamo trovare underscore _ */
+greetings(); // chiamo la funzione
+console.log(pow(4)); // 16
