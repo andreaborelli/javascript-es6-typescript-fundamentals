28a01ed30eb472800cb6e5bb1831f9a442d6536e
Andrea Borelli <andreaborelli@blu.it>
2024-07-25 18:43:05 +0200

    Array: find & findIndex

--- script.js
+++ script.js
@@ -1,27 +1,21 @@
 const users = [
-  {"id": 1, "name": "Silvia", "age": 2, "gender": "F", city: "Gorizia"},
-  {"id": 2, "name": "Fabio", "age": 40, "gender": "M", city: "Trieste"},
-  {"id": 3, "name": "Lorenzo", "age": 6, "gender": "M", city: "Pordenone"},
-  {"id": 4, "name": "Lisa", "age": 40, "gender": "F", city: "Gorizia"}
+  {"id": 10, "name": "Silvia", "age": 2, "gender": "F", city: "Gorizia"},
+  {"id": 20, "name": "Fabio", "age": 40, "gender": "M", city: "Trieste"},
+  {"id": 30, "name": "Lorenzo", "age": 6, "gender": "M", city: "Pordenone"},
+  {"id": 40, "name": "Lisa", "age": 40, "gender": "F", city: "Gorizia"}
 ];
 
-/* il metodo filter degli array ci permette di filtrare una collezione, 
-quindi di partire di un array e arrivare a restituire un nuovo array, 
-quindi ci restituirà una nuova collezione senza alcun riferimento alla precedente,
-andando ad iterare su ogni singola voce della collezione, 
-è andando a definire con un predicato, 
-ovvero un espressione che ci restituisce un booleano true o false, 
-se quello elemento sarà incluso nella collezione con un true oppure escluso con un false*/
+/* Il metodo findIndex tramite un funzione ci permette di andare a recuperare 
+l'indice di un elemento della nostra collezione  */
 
-//const newList = users.filter(user =>{
-	// return user.age > 18;
-// })
+const ID = 30
 
+// const index = users.findIndex(user => user.id === ID)
 
-const newList = users.filter(user => user.age > 18)
-					.map(user => user.id); // per ogni elemento filtrato a noi interessa esclusivamente l'id
-// anche il map ci permette di iterare sugli elementi filtrati  
+// users.splice(index, 1);
 
-// risultato sarà un array solo con gli id che corrispondono alla nostra condizione user.age > 18
+const user = users.find(user => user.id === ID)
 
-console.log(newList); // [2, 4]
+console.log(user); // { id: 30, name: 'Lorenzo', age:
+
+/* Il metodo find permette di cercare direttamente l'elemento che stiamo cercando */
