13b274d033a9c8d09bbf038f6069d26fe90298d3
Andrea Borelli <andreaborelli@blu.it>
2024-07-31 17:30:40 +0200

    Import modules e type="module"

---
+++ main.js
@@ -0,0 +1,3 @@
+import { a, add, divide } from './utility.js';
+
+console.log(add(1, 2))
---
+++ utility.js
@@ -0,0 +1,10 @@
+
+export const a = 1;
+
+export const add = (a, b) => {
+  return a + b;
+};
+
+export const divide = function (a, b) {
+  return a / b;
+};
