4faf577740e91e128e3f43a3d69b37e40e5f92cb
Andrea Borelli <andreaborelli@blu.it>
2024-08-27 18:00:50 +0200

    Import as

--- main.js
+++ main.js
@@ -1,3 +1,6 @@
-import { a, add, divide } from './utility.js';
+import { add } from './utility.js';
+import { add as newAdd } from './operations.js';
 
-console.log(add(1, 2))
+console.log(add(1, 4));
+console.log(newAdd(5));
+
--- utility.js
+++ utility.js
@@ -1,10 +1,4 @@
 
-export const a = 1;
-
 export const add = (a, b) => {
   return a + b;
-};
-
-export const divide = function (a, b) {
-  return a / b;
 };
