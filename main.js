45f6a6785f2098a45475f8af393187a0802affcc
Andrea Borelli <andreaborelli@blu.it>
2024-08-29 16:12:48 +0200

    Promises

--- main.js
+++ main.js
@@ -1,6 +1,22 @@
 import Panel  from './utility.js';
 
-const p = new Panel();
+const a = new Panel();
 
 
-/* default, predefinito ne possiamo mettere uno per modulo è toglie le parerntesi {} da import*/
+const p = new Promise((resolve, reject) => {
+    setTimeout(() => resolve(5), 3000);
+});
+
+p.then(
+    () => new Promise((resolve, reject) => {
+        setTimeout(() => resolve(50), 3000);
+    })
+).then(
+    data => console.log(data)
+).catch(
+    err => console.log(err)
+);
+
+/* una promise è un oggetto che rappresenta un'operazione asincrona e il suo eventuale completamento o fallimento. 
+Le promise sono un modo per gestire il codice asincrono, facilitando il lavoro con operazioni 
+che non avvengono immediatamente, come richieste HTTP, timeout, o operazioni di lettura/scrittura su file. */
